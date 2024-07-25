b9e63dabd86fdf7dc1d2ff564e7fed0399124c60
nimishgo <nimish.2711@gmail.com>
2024-07-25 16:24:35 +0530

    feat : basic  server template

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
